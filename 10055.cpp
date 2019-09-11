e265c0637afc4f07dc2fd7040fd3bec15c47b744
yxuan0329 <46278823+yxuan0329@users.noreply.github.com>
2019-09-12 00:06:34 +0800

    Add files via upload

---
+++ 10055.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int num1, num2, ans=0;
+    cin >>num1>>num2;
+    ans =abs(num1-num2);
+    cout<<ans;
+    return 0;
+}
