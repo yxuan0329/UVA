3414a5fc52a912cb5a2066efeee0f1d50d6cceda
YunXuan <46278823+yxuan0329@users.noreply.github.com>
2021-02-18 23:57:09 +0800

    Update p11689.cpp

--- p11689.cpp
+++ p11689.cpp
@@ -9,11 +9,13 @@ int main()
     cin >>n;
     while(n--){
         cin >> e>> f>> c ;
-        int sodas = 0;//how many bottles drank
+        int sodas = 0, remain=0; //how many bottles drank, remain
         while((e+f)>=c){
             sodas += (e+f)/c;
-            e = (e+f)/c;
+            remain = (e+f)/c;
             f = (e+f)%c;
+            e = remain;
+            cout<<"sodas="<<sodas<<",e="<<e<<",f="<<f<<endl;
         }
         cout<<sodas<<endl;
     }
